e17f890b67a47926a60e3f48a46cbe59ce8bb503
AngelVm20 <84336462+AngelVm20@users.noreply.github.com>
2022-05-09 15:47:58 -0500

    dependencias agregadas

---
+++ aviones-web/src/main/java/com/aviones/web/avionesweb/AvionesWebApplication.java
@@ -0,0 +1,13 @@
+package com.aviones.web.avionesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvionesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvionesWebApplication.class, args);
+	}
+
+}
---
+++ aviones-web/src/test/java/com/aviones/web/avionesweb/AvionesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviones.web.avionesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvionesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
