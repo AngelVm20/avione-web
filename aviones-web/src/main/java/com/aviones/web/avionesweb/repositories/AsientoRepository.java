0deb2f388055fdf93af1ef42c5c7aae5e3ef0b68
AngelVm20 <84336462+AngelVm20@users.noreply.github.com>
2022-05-18 14:30:30 -0500

    avance miercoles

---
+++ aviones-web/src/main/java/com/aviones/web/avionesweb/repositories/AsientoRepository.java
@@ -0,0 +1,11 @@
+package com.aviones.web.avionesweb.repositories;
+
+import com.aviones.web.avionesweb.models.Asiento;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AsientoRepository extends JpaRepository<Asiento,Long>{
+    
+}
